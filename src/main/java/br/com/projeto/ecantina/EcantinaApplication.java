7870de577ed53d18b727d598e7870c7731ff9e40
LucasAuSilva <silvaaugustolucas@gmail.com>
2021-05-11 18:25:04 -0300

    first commit

---
+++ src/main/java/br/com/projeto/ecantina/EcantinaApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.ecantina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcantinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcantinaApplication.class, args);
+	}
+
+}
