3854038dc50d26227c14faa1537f199cc3dc9272
Kenzo De Albuquerque <64553441+OzneKx@users.noreply.github.com>
2021-05-13 00:14:02 -0300

    Models e Relations criada com sucesso

--- src/main/java/br/com/projeto/ecantina/models/Establishment.java
+++ src/main/java/br/com/projeto/ecantina/models/Establishment.java
@@ -9,7 +9,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity(name = "establishments")
-public class Establishment extends User{
+public class Establishment extends User {
     
     @Column(length = 18)
     private String cnpj;
@@ -26,10 +26,52 @@ public class Establishment extends User{
     @OneToOne(mappedBy = "establishment")
     private Address address;
 
+    
     public Establishment() {}
 
     public Establishment(String email, String password, String name, Integer capacity) {
         super(email, password, name);
         this.capacity = capacity;
+    }
+
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public BigDecimal getRent() {
+        return rent;
+    }
+
+    public void setRent(BigDecimal rent) {
+        this.rent = rent;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+
+    public void setRestaurants(List<Restaurant> restaurants) {
+        this.restaurants = restaurants;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
 }
--- src/main/java/br/com/projeto/ecantina/models/TypeUser.java
+++
@@ -1,8 +0,0 @@
-package br.com.projeto.ecantina.models;
-
-public enum TypeUser {
-    
-    Client, 
-    Restaurant,
-    Establishment,
-}
