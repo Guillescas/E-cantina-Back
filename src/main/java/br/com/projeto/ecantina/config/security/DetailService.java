ae091524ea652df92f64b8c729c33a877be6167b
LucasAuSilva <silvaaugustolucas@gmail.com>
2021-05-13 18:04:47 -0300

    starting security configs

---
+++ src/main/java/br/com/projeto/ecantina/config/security/DetailService.java
@@ -0,0 +1,9 @@
+package br.com.projeto.ecantina.config.security;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DetailService implements UserDetailsService {
+    
+}
