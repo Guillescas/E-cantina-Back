71c1ec46fce0f7a6684f55e25970c8eddb9f6602
LucasAuSilva <silvaaugustolucas@gmail.com>
2021-05-18 16:49:06 -0300

    handler for invalidDataAcessResourceUsageException

--- src/main/java/br/com/projeto/ecantina/config/errors/ErrorHandlers.java
+++ src/main/java/br/com/projeto/ecantina/config/errors/ErrorHandlers.java
@@ -1,28 +1,35 @@
 package br.com.projeto.ecantina.config.errors;
 
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import br.com.projeto.ecantina.config.validation.ResponseRegisterError;
-
 @RestControllerAdvice
 public class ErrorHandlers {
 
-    // TODO InvalidDataAccessResourceUsageException handler
-    
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
     @ExceptionHandler(value = NullPointerException.class)
-    public ResponseRegisterError handle(NullPointerException exception) {
+    public ResponseErrors handle(NullPointerException exception) {
 
-        String msg = exception.getMessage();
+        String msg = exception.toString();
         if(msg.contains(":")) {
             String[] error = msg.split(":");
-            return new ResponseRegisterError(error[0], error[1]);
+            return new ResponseErrors(error[0], error[1]);
         } else {
-            return new ResponseRegisterError("Error", msg);
+            return new ResponseErrors("Error", msg);
         }
 
+    }
+
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value = InvalidDataAccessResourceUsageException.class)
+    public ResponseErrors handle(InvalidDataAccessResourceUsageException exception) {
+
+        String msg = exception.getMessage();
+        exception.printStackTrace();
+
+        return new ResponseErrors("Error", msg);
     }
 }
